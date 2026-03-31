6df65913a6780511dcfe8c8df578f1104d3477bc
dismox <dismoxem@gmail.com>
2026-03-31 23:37:29 +0700

    получение данных об уровне

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -25,21 +25,44 @@ func _on_level_button_pressed() -> void:
 	
 func fill_level_preview():
 	
+	#как-то передавать айди уровня нужного пока не знаю как, но пока наверное вручную вставить просто
+	var level_id = ""
+	await http_request.request("http://192.168.56.1:80/tasks/" + level_id + "/leaderboard", ["Content-Type: application/json"], HTTPClient.METHOD_GET)
+
+
+
 	#тестовые данные
-	var data_array = [
-		{ "name": "name1", "score": "1", "time": "00:00" },
-		{ "name": "name2", "score": "42", "time": "01:15" }
-	]
+	#var data_array = [
+	#	{ "name": "name1", "score": "1", "time": "00:00" },
+	#	{ "name": "name2", "score": "42", "time": "01:15" }
+	#]
 	
-	#указать правильный url
-	#как-то передавать айди уровня нужного пока не знаю как
-	await http_request.request("http://192.168.56.1:80/...", ["Content-Type: application/json"], HTTPClient.METHOD_GET)
-	
-	for item in data_array:
-		var name = str(item.get("name", "?"))
-		var score = str(item.get("score", "0"))
-		var time = str(item.get("time", "00:00"))
 		
-		var table_line: TableLine = load("res://table_line.tscn").instantiate()
-		leader_table.add_child(table_line)
-		table_line.set_values(name, time, score)
+	#for item in data_array:
+	#	var name = str(item.get("name", "?"))
+	#	var score = str(item.get("score", "0"))
+	#	var time = str(item.get("time", "00:00"))
+	#	
+	#	var table_line: TableLine = load("res://table_line.tscn").instantiate()
+	#	leader_table.add_child(table_line)
+	#	table_line.set_values(name, time, score)
+
+
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if response_code == 0:
+		print("Привышено время ожидания ответа")
+		
+	elif response_code == 200:
+		var json_string = body.get_string_from_utf8()
+		var data = JSON.parse_string(json_string)
+		
+		for item in data:
+			var name = str(item.get("name", "?"))
+			var score = str(item.get("score", "0"))
+			var time = str(item.get("time", "00:00"))
+			
+			var table_line: TableLine = load("res://table_line.tscn").instantiate()
+			leader_table.add_child(table_line)
+			table_line.set_values(name, time, score)
+	else:
+		print(body.get_string_from_utf8())
